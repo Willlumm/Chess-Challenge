a2dbf5f3e0277d2d4c3228ec6a369d87fde69811
Willlumm <william.lumme@gmail.com>
2023-07-29 22:03:22 +0100

    Outline evaluation and think functions.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,29 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    public static float Eval(Board board)
+    {
+        return 0;
+    } 
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move best_move = moves[0];
+        float best_eval = -100;
+        foreach(Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+            float eval = Eval(board);
+            if(eval > best_eval)
+            {
+                best_move = move;
+                best_eval = Eval(board);
+            }
+            board.UndoMove(move);
+        }
+        return best_move;
     }
 }
